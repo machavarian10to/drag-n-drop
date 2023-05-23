90fc497f3ab949e483702f44a9106a0289ac900e
Oto Machavariani <machavarian10to>
2023-05-23 19:49:53 +0400

    feat: complete full  version

--- script.js
+++ script.js
@@ -26,50 +26,25 @@ function dropCard(event) {
     if(!isContingenciesTrue) return;
 
     // Append card to dropzone
+    const cardOrder = card.getAttribute("data-order");
     if(cardId === "liveness-check" || cardId === "video-call-verification"){
-        appendRadioCard(cardId, card, event)
+        appendRadioCard(cardId, cardOrder, card, event)
     }else {
         const cardText = card.innerText;
-        appendDefaultCard(cardId, cardText, event);
+        appendDefaultCard(cardId, cardOrder, cardText, event);
     }
 
     // Check if there is a card above the newly dropped card and update card's svg
     const droppedCards = Array.from(dropZone.querySelectorAll('.dropped-card'));
-    const droppedCardIndex = droppedCards.findIndex(droppedCard => droppedCard.id === cardId);
-    const aboveCard = droppedCards[droppedCardIndex - 1];
-    if (aboveCard) aboveCard.querySelector(".cards-connection").style.display = "block";
+    droppedCards.forEach((droppedCard, index) => {
+    if (index !== droppedCards.length - 1) {
+        droppedCard.querySelector(".cards-connection").style.display = "block";
+    }});
     scaleDropzone(droppedCards);
 }
 
 function checkContingencies(card, id){
-    if(id === "id-verification"){
-        const dropZoneCard = dropZone.querySelector("#id-in-selfie-verification");
-
-        // Checks if ID-in-Selfie is dropped replace it with ID verification and its dependencies
-        if(dropZoneCard) {
-            const idInSelfieVerify = document.querySelector("#id-in-selfie-verification");
-            idInSelfieVerify.classList.remove("dropped-card");
-            disableCardStyles(idInSelfieVerify);
-
-            const faceMatch = dropZone.querySelector("#face-match");
-            const livenessCheck = dropZone.querySelector("#liveness-check");
-            if(faceMatch && !livenessCheck){
-                const card = document.querySelector("#face-match");
-                disableCardStyles(card);
-                card.classList.remove('dropped-card');
-                dropZone.removeChild(faceMatch);
-            }
-
-            const droppedCards = Array.from(dropZone.querySelectorAll('.dropped-card'));
-            const lastCard = droppedCards[droppedCards.length - 1];
-            if(lastCard) lastCard.querySelector(".cards-connection").style.display = "none";
-            scaleDropzone(droppedCards);
-            dropZone.removeChild(dropZoneCard);
-        } 
-    }else if(id === "id-in-selfie-verification"){
-        const idVerify = dropZone.querySelector("#id-verification");
-        if(idVerify) return false;
-    }else if(id === "id-databases-check" || id === "aml-screening" || id === "age-verification"){
+    if(id === "id-databases-check" || id === "aml-screening" || id === "age-verification"){
         const idInSelfieVerify = dropZone.querySelector("#id-in-selfie-verification");
         const idVerify = dropZone.querySelector("#id-verification");
         if(!idInSelfieVerify && !idVerify) return false;
@@ -90,9 +65,9 @@ function checkContingencies(card, id){
 }
 
 
-function appendDefaultCard(id, text, e) {
-    const card = `
-      <div class="card dropped-card" id="${id}" ondrop="preventDrop(event)">
+function appendDefaultCard(id, order, text, e) {
+    const html = `
+      <div class="card dropped-card" id="${id}" data-order="${order}" ondrop="preventDrop(event)">
           <div class="rectangle">
               <svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M11.75 15H11.6C10.3399 15 9.70982 15 9.22852 14.7548C8.80516 14.539 8.46095 14.1948 8.24524 13.7715C8 13.2902 8 12.6601 8 11.4V6.6C8 5.33988 8 4.70982 8.24524 4.22852C8.46095 3.80516 8.80516 3.46095 9.22852 3.24524C9.70982 3 10.3399 3 11.6 3H11.75M11.75 15C11.75 15.8284 12.4216 16.5 13.25 16.5C14.0784 16.5 14.75 15.8284 14.75 15C14.75 14.1716 14.0784 13.5 13.25 13.5C12.4216 13.5 11.75 14.1716 11.75 15ZM11.75 3C11.75 3.82843 12.4216 4.5 13.25 4.5C14.0784 4.5 14.75 3.82843 14.75 3C14.75 2.17157 14.0784 1.5 13.25 1.5C12.4216 1.5 11.75 2.17157 11.75 3ZM4.25 9L11.75 9M4.25 9C4.25 9.82843 3.57843 10.5 2.75 10.5C1.92157 10.5 1.25 9.82843 1.25 9C1.25 8.17157 1.92157 7.5 2.75 7.5C3.57843 7.5 4.25 8.17157 4.25 9ZM11.75 9C11.75 9.82843 12.4216 10.5 13.25 10.5C14.0784 10.5 14.75 9.82843 14.75 9C14.75 8.17157 14.0784 7.5 13.25 7.5C12.4216 7.5 11.75 8.17157 11.75 9Z" stroke="#591EFF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -121,12 +96,15 @@ function appendDefaultCard(id, text, e) {
           </div>
       </div>
     `;
-    e.target.insertAdjacentHTML('beforeend', card);
+    checkOrder(e, html, order);
 }
 
-function appendRadioCard(cardId, card, e){
+function appendRadioCard(cardId, order, card, e){
     const disabledInputs = card.querySelectorAll('input[disabled]');
-    disabledInputs.forEach(input => input.removeAttribute('disabled'));
+    disabledInputs.forEach((input, i) => {
+        if(i === 0) input.checked = true;
+        input.removeAttribute('disabled');
+    });
 
     const cardTexts = card.innerText.split("\n");
     let [cardTitle, firstRadioText, secondRadio] = cardTexts;
@@ -136,7 +114,7 @@ function appendRadioCard(cardId, card, e){
     if(cardId === "video-call-verification") groupName = "video-call";
 
     const html = `
-    <div class="card dropped-card radio-options" id="${cardId}" ondrop="preventDrop(event)">
+    <div class="card dropped-card radio-options" id="${cardId}" data-order="${order}" ondrop="preventDrop(event)">
         <div class="workflow-frame">
             <div class="rectangle">
                 <svg width="16" height="18" viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -153,7 +131,7 @@ function appendRadioCard(cardId, card, e){
 
         <div class="radio-buttons">
             <div class="radios">
-                <input type="radio" name="${groupName}" id="${firstRadioId}" onchange="onRadioChange(event)" />
+                <input type="radio" name="${groupName}" id="${firstRadioId}" onchange="onRadioChange(event)"  checked />
                 <label for="${firstRadioId}">${firstRadioText}</label>
             </div>
 
@@ -179,9 +157,56 @@ function appendRadioCard(cardId, card, e){
       </div>
     </div>
     `;
-    e.target.insertAdjacentHTML('beforeend', html);
+    checkOrder(e, html, order);
+}
+
+function checkOrder(e, html, order) {
+    const droppedCards = dropZone.querySelectorAll('div[data-order]');
+    if (droppedCards.length === 0) {
+      insertHtml(e.target, 'beforeend', html);
+    } else {
+      const cardOrders = Array.from(droppedCards, card => parseInt(card.getAttribute('data-order')));
+  
+      if (cardOrders.length === 1) {
+        handleSingleCardOrder(e, html, order, cardOrders[0]);
+      } else {
+        handleMultipleCardOrders(e, html, order, cardOrders);
+      }
+    }
 }
   
+function handleSingleCardOrder(e, html, order, dataOrder) {
+    if (order > dataOrder) {
+      insertHtml(e.target, 'beforeend', html);
+    } else {
+      const card = dropZone.querySelector(`div[data-order='${dataOrder}']`);
+      card.insertAdjacentHTML('beforebegin', html);
+    }
+}
+  
+function handleMultipleCardOrders(e, html, order, cardOrders) {
+    let dataOrder = cardOrders[cardOrders.length - 1];
+    if (order > dataOrder) {
+      insertHtml(e.target, 'beforeend', html);
+    } else {
+      if (order < cardOrders[0]) {
+        dataOrder = cardOrders[0];
+      } else {
+        for (let i = 0; i < cardOrders.length - 1; i++) {
+          if (order > cardOrders[i] && order < cardOrders[i + 1]) {
+            dataOrder = cardOrders[i + 1];
+            break;
+          }
+        }
+      }
+      const card = dropZone.querySelector(`div[data-order='${dataOrder}']`);
+      card.insertAdjacentHTML('beforebegin', html);
+    }
+}
+  
+function insertHtml(element, position, html) {
+    element.insertAdjacentHTML(position, html);
+}
 
 // Disables a card and removes it from the drop zone
 function disableCard(event) {
@@ -217,9 +242,7 @@ function checkDependencies(id){
         const faceMatch = dropZone.querySelector("#face-match");
 
         if(idDatabasesCheck){
-            if(idInSelfieVerify || idVerify){
-                pass;
-            }else {
+            if(!idInSelfieVerify && !idVerify){
                 const card = document.querySelector("#id-databases-check");
                 disableCardStyles(card);
                 card.classList.remove('dropped-card');
@@ -228,9 +251,7 @@ function checkDependencies(id){
         }
 
         if(amlScreening){
-            if(idInSelfieVerify || idVerify){
-                pass;
-            }else {
+            if(!idInSelfieVerify && !idVerify){
                 const card = document.querySelector("#aml-screening");
                 disableCardStyles(card);
                 card.classList.remove('dropped-card');
@@ -239,9 +260,7 @@ function checkDependencies(id){
         }
 
         if(ageVerify){
-            if(idInSelfieVerify || idVerify){
-                pass;
-            }else {
+            if(!idInSelfieVerify && !idVerify){
                 const card = document.querySelector("#age-verification");
                 disableCardStyles(card);
                 card.classList.remove('dropped-card');
@@ -292,7 +311,6 @@ function scaleDropzone(cards){
     const scale = totalHeight > dropZone.offsetHeight / 2 ? dropZone.offsetHeight / (totalHeight * 2) : 1;
     dropZone.style.transform = `scale(${scale})`;
 }
-
 
 function onRadioChange(event) {
     const radioId = event.target.id;
